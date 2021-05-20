7689f16d3030305b289bf9e21c02448e55bd3805
Suvash Shah Thakuri <suvash@appmozart.com>
2021-05-20 12:17:30 -0700

    User class updated

--- euro-bet/Models/User.cs
+++ euro-bet/Models/User.cs
@@ -3,6 +3,7 @@ namespace euro_bet.Models
     public class User: Person
     {
         public int UserID { get; set; }
+        public Account Account { get; set; }
         public Contact Contact { get; set; }
         public Balance Balance { get; set; }
 
