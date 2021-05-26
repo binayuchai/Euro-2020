21cd08d3811148497bfeca6adeeca0fb528c34c4
Suvash Shah Thakuri <suvash@appmozart.com>
2021-05-26 16:02:07 -0700

    Schedule page added

---
+++ euro-bet/Controllers/ScheduleController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using euro_bet.Models;
+
+namespace euro_bet.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private readonly ILogger<ScheduleController> _logger;
+
+        public ScheduleController(ILogger<ScheduleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
